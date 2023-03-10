48307207a5219118c95dcbefd08187060ac5288f
vipulchaudhary16 <vipulr6111@gmail.com>
2023-03-10 22:06:46 +0530

    user can update and delete application

--- routes/Application.js
+++ routes/Application.js
@@ -27,13 +27,41 @@ router.post('/add', verifyUser, async (req, res) => {
   }
 });
 
+router.put('/update/:id', verifyUser, async (req, res) => {
+  try {
+    const application = await Application.findById(req.params.id);
+    if (application.userId === req.user.id) {
+      await Application.findByIdAndUpdate(req.params.id, req.body);
+      res.json({ "message": "application updated" })
+    } else {
+      res.status(401).json({ "message": "unauthorized" })
+    }
+  } catch (error) {
+    res.status(500).json({ "message": "Internal server error" })
+  }
+})
+
+router.delete('/delete/:id', verifyUser, async (req, res) => {
+  try {
+    const application = await Application.findById(req.params.id);
+    if (application.userId === req.user.id) {
+      await Application.findByIdAndDelete(req.params.id);
+      res.json({ "message": "application deleted" })
+    } else {
+      res.status(401).json({ "message": "unauthorized" })
+    }
+  } catch (error) {
+    res.status(500).json({ "message": "Internal server error" })
+  }
+})
+
 router.get('/get-all', verifyUser, async (req, res) => {
   try {
     const userId = req.user.id //provide by middleware
     const applications = await Application.find({ userId: userId })
     res.json(applications);
   } catch (error) {
-    res.status(5000).json({message: "Internal server error"})
+    res.status(5000).json({ message: "Internal server error" })
   }
 })
 
